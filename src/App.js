49d6cb923044424416ff83746d6f9ff34d0a9105
Fin Mavis <fin.mavis@yahoo.com>
2018-07-10 14:07:13 +0700

    Adding Footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Aux from './components/AuxWrap';
 import Header from './components/Header';
 import Calculator from './containers/Calculator';
+import Footer from './components/Footer';
 
 class App extends Component {
   state = {
@@ -16,6 +17,7 @@ class App extends Component {
           icon="fas fa-calculator"
           title={this.state.title} />
         <Calculator />
+        <Footer />
       </Aux>
     );
   }
